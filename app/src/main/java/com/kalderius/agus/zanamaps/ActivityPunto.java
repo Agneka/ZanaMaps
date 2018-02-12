1422f74d7f971100e477643eb987cfd77cf335c7
duckiermango <miguelangelarocas@gmail.com>
2018-02-12 16:47:18 +0100

    Activity-Punto - MIGUE

---
+++ app/src/main/java/com/kalderius/agus/zanamaps/ActivityPunto.java
@@ -0,0 +1,13 @@
+package com.kalderius.agus.zanamaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityPunto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_punto);
+    }
+}
