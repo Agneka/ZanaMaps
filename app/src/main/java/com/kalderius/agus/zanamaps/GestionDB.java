ae64defbeabb32c27ca1089a5b1c2226a75f2b16
David <pola_545@hotmail.com>
2018-02-15 16:54:25 +0100

    Clase GestionDB comentada

--- app/src/main/java/com/kalderius/agus/zanamaps/GestionDB.java
+++ app/src/main/java/com/kalderius/agus/zanamaps/GestionDB.java
@@ -14,48 +14,68 @@ import java.util.List;
 public class GestionDB {
     private SQLiteDatabase db;
 
+    /**
+     * Constructor en el cual se abre la base de datos privada PuntosDB o la genera si no existe
+     * Ademas de crear la tabla Puntos en caso de que no exista
+     * @param act
+     */
     public GestionDB(ActivityMain act) {
         this.db=act.openOrCreateDatabase("PuntosDB",act.MODE_PRIVATE,null);
-        crearTabla();
+        crearTabla();//metodo encargado de crear la tabla si no existe
     }
     public void crearTabla(){
-        String crear="CREATE TABLE IF NOT EXISTS Puntos(id INTEGER PRIMARY KEY, nombre VARCHAR(100),coorX VARCHAR(250), coorY VARCHAR(250), visitado INTEGER );";
-        db.execSQL(crear);
+        String crear="CREATE TABLE IF NOT EXISTS Puntos(id INTEGER PRIMARY KEY, nombre VARCHAR(100),coorX VARCHAR(250), coorY VARCHAR(250), visitado INTEGER );";//sql de creacion de tabla
+        db.execSQL(crear);//ejecucion del sql
 
     }
+
+    /**
+     * Metodo encargado de insertar un Punto pasado por parametro a la tabla Puntos de la base de datos
+     * @param punto
+     */
     public void insertar(Punto punto){
-        String visitado;
-        if (punto.isVisitado()){
+        String visitado;//string el cual se insertara en la columna visitado
+        if (punto.isVisitado()){//si ha sido visitado
             visitado="1";
-        }else {
+        }else {//sino
             visitado="0";
         }
-        String select ="SELECT COUNT(*) FROM Puntos;";
-        Cursor c=db.rawQuery(select,null);
-        c.moveToFirst();
-        String id=c.getString(0);
-        punto.setId(Integer.parseInt(id));
-        c.close();
+        String select ="SELECT COUNT(*) FROM Puntos;";//select del contador de puntos existentes para generar siguiente id(autoincremental)
+        Cursor c=db.rawQuery(select,null);//cursor que contiene las filas de la consulta select
+        c.moveToFirst();//como el cursor solo tiene una fila no hace falta hacer un bucle
+        String id=c.getString(0);//cogemos el primer y unico resultado de la fila que sera el id del punto a insertar
+        punto.setId(Integer.parseInt(id));//le damos el id al objeto punto
+        c.close();//cerramos cursor
         String insert= "INSERT INTO Puntos VALUES("+
                 id + "," +
                 "'" + punto.getNombre() + "'," +
                 "'" + punto.getCoorx() + "'," +
                 "'" + punto.getCoory() + "'," +
-                visitado + ");";
-        db.execSQL(insert);
+                visitado + ");";//Sentencia Insert del punto dado
+        db.execSQL(insert);//ejecutar insert
 
     }
+
+    /**
+     * metodo encargado de borrar de la BD el punto dado por parametro
+     * @param punto
+     */
     public void eliminar(Punto punto){
-        String delete="DELETE FROM Puntos WHERE coorX='" + punto.getCoorx() + "' AND coorY='"+ punto.getCoory() + "';";
-        db.execSQL(delete);
+        String delete="DELETE FROM Puntos WHERE coorX='" + punto.getCoorx() + "' AND coorY='"+ punto.getCoory() + "';";//Sentencia de delete del punto dado
+        db.execSQL(delete);//ejecutar delete
     }
+
+    /**
+     * metodo encargado de recorrer las filas de la tabla Puntos
+     * @return ArrayList lista el cual contiene Objetos Punto
+     */
     public ArrayList recuperarPuntos(){
-        String select="Select * from Puntos;";
-        Punto p;
-        ArrayList lista=new ArrayList();
-        String[] atributos=new String[5];
+        String select="Select * from Puntos;";//Consulta select
+        Punto p;//objeto contenedor de puntos a agregar a la lista
+        ArrayList lista=new ArrayList();//lista que devolveremos
+        String[] atributos=new String[5];//contenedor de atributos del objeto Punto
         Cursor c=db.rawQuery(select,null);
-        if (c.moveToFirst()){
+        if (c.moveToFirst()){//si el cursor contiene datos
             int i= 0;
             while (i< c.getCount()){
                 atributos[0]=c.getString(c.getColumnIndex("id"));
@@ -63,21 +83,26 @@ public class GestionDB {
                 atributos[2]=c.getString(c.getColumnIndex("coorX"));
                 atributos[3]=c.getString(c.getColumnIndex("coorY"));
                 atributos[4]=c.getString(c.getColumnIndex("visitado"));
-                if (atributos[4] == "0") {
+                if (atributos[4] == "0") {//si es cero el atributo visitado sera false
                     p = new Punto(atributos[1], atributos[2], atributos[3], false);
-                }else {
+                }else {//si no sera true
                     p = new Punto(atributos[1], atributos[2], atributos[3], true);
                 }
-                lista.add(p);
-                c.moveToNext();
-                i++;
+                lista.add(p);//Se agrega el punto generado a la lista
+                c.moveToNext();//Se mueve al siguiente registro del cursor
+                i++;//se incremente el iterador del bucle
             }
         }
-        c.close();
-        return lista;
+        c.close();//se cierra el cursor
+        return lista;//devuelve la lista
     }
+
+    /**
+     * Actualiza el punto dado y lo pone como visitado
+     * @param punto
+     */
     public void marcarVisto(Punto punto){
-        String update="UPDATE Puntos SET visitado=1 where id=" + punto.getId() + ";";
-        db.execSQL(update);
+        String update="UPDATE Puntos SET visitado=1 where id=" + punto.getId() + ";";//sentencia update del punto dado
+        db.execSQL(update);//ejecutar update
     }
 }
